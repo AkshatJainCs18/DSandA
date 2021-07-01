15cf80db3d9a0bc5c95b2241088b43e782e1175d
AkshatJainCs18 <akshatjaincs18@gmail.com>
2021-07-01 10:05:32 +0530

    MaxOnAddingOrNot

--- 30June/toffees.cpp
+++ 30June/toffees.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-int n;  cin>>n;
-vector<int>kids;
-for(int i=0;i<n;++i){
-    int k;cin>>k;
-    kids.push_back(k);
-}
-int toffees;
+int n,toffees;  cin>>n;
+int kids[n];
+for(int i=0;i<n;++i)   cin>>kids[i];
 cin>>toffees;
-for(int i=0;i<kids.size();++i){
-    int maxele=*max_element(kids.begin(),kids.end());
+for(int i=0;i<n;++i){
+    int maxele=*max_element(kids,kids+n);
     if(kids[i]+toffees>=maxele)  cout<<"true"<<" ";
     else    cout<<"false"<<" ";
 }
